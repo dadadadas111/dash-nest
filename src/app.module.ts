f183ed65f33e15f62c6ab05341cdc30aeb8b9104
Nguyen Thanh Long <nguyenthanhlong12102004vp@gmail.com>
2026-01-28 12:01:00 +0700

    style: format all files

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,4 +42,4 @@ import { ChecklistModule } from './features/checklist/checklist.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
